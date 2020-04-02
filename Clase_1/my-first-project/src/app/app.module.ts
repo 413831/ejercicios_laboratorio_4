531271a005f797a5fb9b5c29f83bb94e9e433bdf
413831 <alepleins3pp@gmail.com>
2020-04-02 09:46:08 -0300

    Clase 1 - Proyecto AngularJs

---
+++ Clase_1/my-first-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { AppRoutingModule } from './app-routing.module' ;
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './componentes/login/login.component' ;
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule ,
+    AppRoutingModule ,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
