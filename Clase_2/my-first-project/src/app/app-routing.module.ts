7eac3c7702821e1c7a2713b1ac5ec8db8e9f5868
413831 <alepleins3pp@gmail.com>
2020-04-02 20:24:12 -0300

    Segunda clase

---
+++ Clase_2/my-first-project/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
+
+const routes: Routes = [
+  {path: 'bienvenida', component: BienvenidaComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: BienvenidaComponent},
+  {path: 'error', component: ErrorComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
